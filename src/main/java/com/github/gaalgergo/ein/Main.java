8f27a271f29e50504a760edd70ac8a0b0a64c3a9
gaalgergo <gergo0122@gmail.com>
2018-10-28 19:52:36 +0100

    Spring Boot beállítása.

--- src/main/java/com/github/gaalgergo/ein/Main.java
+++
@@ -1,5 +0,0 @@
-package com.github.gaalgergo.ein;
-
-public class Main {
-    public static void main(final String... args) { }
-}
---
+++ src/main/java/com/github/gaalgergo/ein/app/EinApp.java
@@ -0,0 +1,11 @@
+package com.github.gaalgergo.ein.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinApp {
+    public static void main(final String... args) {
+        SpringApplication.run(EinApp.class, args);
+    }
+}
