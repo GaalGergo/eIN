647e33a8b8dc294233afe5739fc96dd6917ce105
gyulaczegledi <Reparatur.css@gmail.com>
2018-11-20 11:51:57 +0100

    EntityScan beállítása, adatbázis séma automatikus újragenerálása indításkor.

--- src/main/java/com/github/gaalgergo/ein/app/spring/EinApp.java
+++ src/main/java/com/github/gaalgergo/ein/app/spring/EinApp.java
@@ -2,8 +2,10 @@ package com.github.gaalgergo.ein.app.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.github.gaalgergo.ein.app")
 public class EinApp {
     public static void main(final String... args) {
         SpringApplication.run(EinApp.class, args);
