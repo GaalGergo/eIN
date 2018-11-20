fe5b6dde9fdac10e7fda4f4cee656d637653581a
czkaroly <czkaroly@gmail.com>
2018-11-20 14:25:22 +0100

    Statikus kódellenőrzők és a Lombok bekötése a projektbe.

--- src/main/java/com/github/gaalgergo/ein/app/kozos/AlapEntity.java
+++ src/main/java/com/github/gaalgergo/ein/app/kozos/AlapEntity.java
@@ -1,10 +1,13 @@
 package com.github.gaalgergo.ein.app.kozos;
 
+import lombok.Data;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+@Data
 @MappedSuperclass
 public class AlapEntity {
 
--- src/main/java/com/github/gaalgergo/ein/app/tanulo/TanuloEntity.java
+++ src/main/java/com/github/gaalgergo/ein/app/tanulo/TanuloEntity.java
@@ -1,11 +1,15 @@
 package com.github.gaalgergo.ein.app.tanulo;
 
 import com.github.gaalgergo.ein.app.kozos.AlapEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "tanulok")
 public class TanuloEntity extends AlapEntity {
