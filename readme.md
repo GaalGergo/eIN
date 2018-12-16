3c4026843f65877b93db718b238e757bfbd0370c
czkaroly <czkaroly@gmail.com>
2018-12-16 23:57:42 +0100

    Readme, war futtatás.

---
+++ readme.md
@@ -0,0 +1,7 @@
+# EIN
+### Elektronikus iskolanapló
+
+#### Projekt indítása
+Fejlesztés: `mvn clean install spring-boot:run`  
+WAR: `java -jar target/ein-0.1.0-SNAPSHOT.war`  
+Böngésző: `localhost:8080`
--- src/main/java/com/github/gaalgergo/ein/app/controller/OldalController.java
+++ src/main/java/com/github/gaalgergo/ein/app/controller/OldalController.java
@@ -6,11 +6,13 @@ import com.github.gaalgergo.ein.app.repository.FelhasznaloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class OldalController {
@@ -27,11 +29,16 @@ public class OldalController {
     }
 
     @GetMapping(path = "/reg")
-    public String reg(final Model model) {
+    public ModelAndView reg() {
+        ModelAndView modelAndView = new ModelAndView("regisztracio");
+        List<String> tipusok = Arrays.stream(FelhasznaloTipus.values())
+                .map(t -> t.name())
+                .collect(Collectors.toList());
+
         FelhasznaloEntity felhasznaloEntity = new FelhasznaloEntity();
-        model.addAttribute("felhasznalo", felhasznaloEntity);
-        model.addAttribute("tipusok", new ArrayList<String>(){{ add("OKTATO"); add("TANULO"); }});
-        return "regisztracio";
+        modelAndView.addObject("felhasznalo", felhasznaloEntity);
+        modelAndView.addObject("tipusok", tipusok);
+        return modelAndView;
     }
 
     @PostMapping(path = "/reg")
