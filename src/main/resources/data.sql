b20ef6750e37ece2b838c7b40ce74f3244dc502a
gaalgergo <gergo0122@gmail.com>
2018-12-17 19:52:14 +0100

    tesztadatok módosítása.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO felhasznalok (azon, nev, felhasznalo_nev, jelszo, tipus)
-VALUES (100, 'Test', 'Test', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.', 'TANULO');
+VALUES (100, 'Teszt Tanulo', 'tanulo1', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.', 'TANULO');
 
 INSERT INTO felhasznalok (azon, nev, felhasznalo_nev, jelszo, tipus)
-VALUES (101, 'Bela', 'bela', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.', 'OKTATO');
+VALUES (101, 'Teszt Oktato', 'oktato1', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.', 'OKTATO');
 
 
 INSERT INTO tantargyak (azon, nev)
